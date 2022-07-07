9e478662505e2eac8fb990fcc187b9eb8565f2e9
Leandro de Mattos <mattoslmp@gmail.com>
2022-07-07 11:36:07 -0300

    Add files via upload

--- run_kaiju2-phyloseq.py
+++ run_kaiju2-phyloseq.py
@@ -3,13 +3,6 @@
 # Autor: Leandro de Mattos Pereira, PhD
 # Pos-doc Vale Institute Technologic
 # Supervisor: Dra. Gisele Nunes
-# This takes a directory with fasta scaffolds generates from Spades and performed kaiju classification (http://kaiju.binf.ku.dk/)
-# Output: kaiju classification, files summary, creates OTU and Tax tables format to be imported into Phyloseq 
-#The programs use kaiju-multi, kaiju2table and kaiju-addTaxonNames. 
-## kaiju-multi, kaiju2table and kaiju-addTaxonNames has to be in your PATH.
-# which is part of the kaiju installation to add taxa names to each OTU.
-# The files names.dmp and nodes.dmp from taxonomy of NCBI and kaiju.fmi (indexed nr ncbi files) are need to Kaiju.
-# kaiju-multi, kaiju2table and kaiju-addTaxonNames has to be in your PATH.
 import sys
 import site; site.getsitepackages
 import argparse
